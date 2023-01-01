8f36ed85b827e220337e134d8d97ea7e01563f00
Ruitong Su <suruitong0428@hotmail.com>
2022-12-31 22:38:17 -0500

    Initial Commit

---
+++ Vapor-iOS/Vapor_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vapor_iOSApp.swift
+//  Vapor-iOS
+//
+//  Created by Ruitong Su on 12/31/22.
+//
+
+import SwiftUI
+
+@main
+struct Vapor_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
